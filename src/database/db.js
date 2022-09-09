5f4156681d7d59cefde1f4fca8142f01421a3244
eltatataa <davidtowo75@gmail.com>
2022-09-09 14:31:39 -0500

    variations in database/db.js

--- src/database/db.js
+++ src/database/db.js
@@ -3,8 +3,8 @@ const colors = require('colors');
 
 (async () => {
     try {
-        await mongoose.connect(process.env.URI);
-        console.log('Conectado a MongoDB'.green);
+        const db = await mongoose.connect(process.env.URI);
+        console.log("Conectado a MongoDB: ".green + `${db.connection.name}`.yellow);
     } catch (error) {
         console.log(`fallo la conexion a MongoDB ${error}`.red);
     }
